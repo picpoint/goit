011a47b4673a9e6934ae08fe7ab728277b05e126
rmtar <picpoint@yandex.ru>
2018-07-03 21:50:27 +0300

    8/8

--- script.js
+++ script.js
@@ -49,7 +49,6 @@ console.log(mul(745, 3));
 
 
 
-
 /*
 +++Задание 3
 Напишите функцию которая принимает аргумент и возвращает функцию которая возвращает этот аргумент.
@@ -98,14 +97,16 @@ console.log(addf(41)(70));
 
 
 
+
 /*
-Задание 5
++++Задание 5
 Напишите функцию которая принимает бинарную функцию и вызывает её имя c аргументами.
 var addf2 = applyf(add);
 console.log(addf2(3)(4)); //7
 console.log(applyf(mul)(3)(4)); //12
 */
 /*
+Решение
 function add(param1) {
 
 	function foo(param2) {
@@ -116,12 +117,21 @@ function add(param1) {
 }
 console.log(add(3)(4));
 
-
-
 function mult(func) {
 	return func;
 }
 console.log(mult(add)(3)(5));
+P.S. Вот моё решение, что я выжал из себя. Подсмотрев в правильное решение, понял что как бы
+только правильно сделал на половину.
+
+Правильное решение
+function applyf(binary) {
+	return function(x) {
+		return function(y) {
+			return binary(x)(y);
+		}
+	}
+}
 */
 
 
@@ -130,8 +140,8 @@ console.log(mult(add)(3)(5));
 
 /*
 Codewars
-
-+++http://www.codewars.com/kata/closures-and-scopes
++++Задание 6
+http://www.codewars.com/kata/closures-and-scopes
 We want to create a function, which returns an array of functions, which return their index in the array.
 For better understanding, here an example:
 var callbacks = createFunctions(5); // create an array, containing 5 functions
@@ -173,6 +183,7 @@ P.S. Я после цикла просто вызвал функцию, пост
 
 
 /*
++++Задание 7
 http://www.codewars.com/kata/can-you-keep-a-secret
 There's no such thing as private properties on a coffeescript object! But, maybe there are?
 Implement a function createSecretHolder(secret) which accepts any value as secret and returns an object with ONLY two methods
@@ -187,17 +198,122 @@ obj.getSecret() # returns 5
 obj.setSecret(2)
 obj.getSecret() # returns 2
 */
+/*
+Решение.
+Здесь я реализовал объект с геттером и сеттером, можно задать значение и значение будет получено.
+Всё здеь как я понимаю работает нормально
+
+var obj = {
+	secret: 5,
+
+	getSecret: function () {
+		return this.secret;
+	},
+
+	setSecret: function (secret) {
+		this.secret = secret;
+	}
+};
+
+console.log(obj.getSecret());
+
+obj.setSecret(10);
+console.log(obj.getSecret());
+*/
+/*
+Здесь же я пробую этот объект запихать в функцию, но я понимаю что работает он не правильно,
+почему неправильно, не пойму.
 
 function createSecretHolder(secret) {
-	get: function () {
-		return secret;
-	}
+
+	var obj = {
+		secret: secret,
+
+		getSecret: function () {
+			return this.secret;
+		},
+
+		setSecret: function (secret) {
+			this.secret = secret;
+		}
+	};
+
+	return obj;
 }
 
-var obj = createSecretHolder(5);
-obj.getSecret();
+console.log(createSecretHolder(777));
+console.log(createSecretHolder('new value'));
+*/
+
+
+
 
 
 /*
++++Задание 8
 http://www.codewars.com/kata/lazily-executing-a-function
+Deferring a function execution can sometimes save a lot of execution time in our programs by postponing the execution
+to the latest possible instant of time, when we're sure that the time spent while executing it is worth it.
+
+Write a method make_lazy that takes in a function (symbol for Ruby) and the arguments to the function and returns
+another function (lambda for Ruby) which when invoked, returns the result of the original function invoked
+with the supplied arguments.
+
+For example:
+
+Given a function add
+
+function add (a, b) {
+  return a + b;
+}
+One could make it lazy as:
+
+var lazy_value = make_lazy(add, 2, 3);
+The expression does not get evaluated at the moment, but only when you invoke lazy_value as:
+
+lazy_value() => 5
+The above invokation then performs the sum.
+
+Please note: The functions that are passed to make_lazy may take one or more arguments and the number of arguments is not fixed.
+
+
+Отсрочка выполнения функции иногда может сэкономить много времени выполнения в наших программах, отложив выполнение
+до самого последнего возможного момента времени, когда мы уверены, что время, затраченное на его выполнение, того стоит.
+Напишите метод make_lazy, который принимает функцию (символ для Ruby) и аргументы функции и возвращает другую функцию
+(лямбда для Ruby), которая при вызове возвращает результат исходной функции, вызванной с помощью аргументов.
+Например: С учетом функции add
+
+function add (a, b) {
+  return a + b;
+}
+
+Можно было бы лениться:
+
+var lazy_value = make_lazy(add, 2, 3);
+Выражение не оценивается в данный момент, но только тогда, когда вы вызываете lazy_value как:
+lazy_value() => 5
+
+Вышеуказанная операция затем выполняет сумму. Обратите внимание: функции, которые передаются make_lazy, могут
+принимать один или несколько аргументов, а количество аргументов не фиксировано.
+*/
+/*
+Решение
+function add() {
+	var result = 0;
+
+	for (var i = 0; i < arguments.length; i++) {
+		result += arguments[i];
+	}
+	return result;
+}
+
+function make_lazy(add) {
+	return add;
+}
+
+var lazy_value = add.bind(add, 5, 15);
+
+console.log(lazy_value());
+
+P.S. Вот как бы решение данного задания опять не знаю, правильно/неправильно
 */
